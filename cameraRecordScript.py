0b3f71bb8d3c08bb8bf4269ae0b51ab14f4987ec
mk <marcin.kapusek@gmail.com>
2021-07-24 02:20:07 +0200

    first draft of a working piece of code

---
+++ cameraRecordScript.py
@@ -0,0 +1,76 @@
+from picamera import PiCamera
+from gpiozero import MotionSensor
+from datetime import datetime
+from time import sleep
+import threading
+from time import time
+
+pir = MotionSensor(17, queue_len = 1, threshold = 0.2, sample_rate = 50)
+camera = PiCamera()
+
+timeout = 15
+
+timeStampLock = threading.Lock()
+currentlyRecordingLock = threading.Lock()
+
+currentlyRecording = False
+
+currentTimeStamp = 10
+
+def dummyFunc():
+    for x in range(0, 2):
+        y = x+2
+
+recordingThread = threading.Thread(target = dummyFunc)
+
+def startRecording(device):
+    global currentlyRecording
+    global currentTimeStamp
+    print('Movement detected..')
+    currentlyRecordingLock.acquire()
+    if not currentlyRecording:        
+        now = datetime.now()
+        filenameRep = now.strftime('%H%M%S_%d%m%Y')
+        camera.start_recording('./' + filenameRep + '.h264')
+        currentlyRecording = True
+        currentlyRecordingLock.release()
+        print('Recording' + filenameRep)
+    else:
+        currentlyRecordingLock.release()
+    timeStampLock.acquire()
+    currentTimeStamp = int(time())
+    timeStampLock.release()
+
+def attemptToStopRecording():
+    global currentlyRecording
+    global currentTimeStamp
+    print('Attempting to stop recording')
+    sleep(timeout)
+    timeStampLock.acquire()
+    if int(time()) - currentTimeStamp >= timeout:
+        timeStampLock.release()
+        currentlyRecordingLock.acquire()
+        if currentlyRecording:
+            camera.stop_recording()
+            currentlyRecording = False
+        currentlyRecordingLock.release()
+        print('Stopped recording')
+    else:
+        timeStampLock.release()
+    print('Attempt finished')
+
+def stopRecording(device):
+    print('No movement is being detected')
+    timeStampLock.acquire()
+    currentTimeStamp = int(time())
+    timeStampLock.release()
+    threading.Thread(target = attemptToStopRecording).start()
+
+pir.when_motion = startRecording
+pir.when_no_motion = stopRecording
+
+while True:
+    sleep(100)
+    #print(('YES' if pir.motion_detected else 'nope')
+    #+ ', val:' +  str(pir.value))
+    #pir.wait_for_motion()
